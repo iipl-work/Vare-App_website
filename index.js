a8a724707362f80c3d5d91144dc3692bbf4d90be
Rishab Rajput <77436170+rishabrajput22@users.noreply.github.com>
2021-10-01 15:17:05 +0530

    up

--- index.js
+++ index.js
@@ -4,65 +4,89 @@ var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://sowmya:iNNrxOhVfEdvsUaI@vare.cnw2n.mongodb.net/vare?retryWrites=true&w=majority";
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.render('newindex', {foo: 'FOO'});
+  res.render('newindex', { foo: 'FOO' });
 });
 app.get('/shopnow', (req, res) => {
 
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("vare");
-  dbo.collection("products").find({}).toArray(function(err, result) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-   // console.log(result);
-    db.close();
-    res.render('listingpage', {"data":result});
+    var dbo = db.db("vare");
+    dbo.collection("products").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      // console.log(result);
+      db.close();
+      res.render('listingpage', { "data": result });
 
+    });
   });
-});
- 
+
 });
 
 app.get('/shopproduct/:id', (req, res) => {
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("vare");
-    dbo.collection("products").findOne({"productId":req.params.id}, function(err, result) {
+    dbo.collection("products").findOne({ "productId": req.params.id }, function (err, result) {
       if (err) throw err;
       console.log(result);
       db.close();
       res.render('product-details-affiliate', result);
     });
   });
-  
+
 });
 app.get('/contact', (req, res) => {
-  res.render('contact-us', {foo: 'FOO'});
+  res.render('contact-us', { foo: 'FOO' });
 });
 app.get('/terms-conditions', (req, res) => {
-  res.render('terms&conditions', {foo: 'FOO'});
+  res.render('terms&conditions', { foo: 'FOO' });
 });
 app.get('/privacy', (req, res) => {
-  res.render('privacypolicy', {foo: 'FOO'});
+  res.render('privacypolicy', { foo: 'FOO' });
 });
 app.get('/aboutus', (req, res) => {
-  res.render('aboutus', {foo: 'FOO'});
+  res.render('aboutus', { foo: 'FOO' });
 });
-app.get('/myaccount', (req, res) => {
-  res.render('my-account', {foo: 'FOO'});
-});
+
 app.get('/404', (req, res) => {
-  res.render('404', {foo: 'FOO'});
+  res.render('404', { foo: 'FOO' });
 });
 app.get('/login-register', (req, res) => {
-  res.render('login-register', {foo: 'FOO'});
+  res.render('login-register', { foo: 'FOO' });
 });
 app.get('/blog', (req, res) => {
-  res.render('blog-04-columns', {foo: 'FOO'});
+  
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("vare");
+    dbo.collection("blog").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      // console.log(result);
+      db.close();
+      res.render('blog-04-columns', { "data": result });
+
+    });
+  });
+
+
 });
 app.get('/singlepostimage', (req, res) => {
-  res.render('single-post-image', {foo: 'FOO'});
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("vare");
+    dbo.collection("blog").findOne({ "_id": req.params.id }, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+      res.render('single-post-image', result);
+    });
+  });
+
+  // res.render('single-post-image', { foo: 'FOO' });
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
